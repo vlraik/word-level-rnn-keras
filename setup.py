fe06470138e6bfacfc7a02e611738491fde6914f
vlraik <vikylakhani@gmail.com>
2016-07-22 00:10:46 +0530

    Package Update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='WordLevelRNN',
+    version='1.0',
+    packages=['wordlevelrnn',],
+    license='	',
+    long_description=open('README.md').read(),
+)
